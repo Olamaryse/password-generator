bbc125bbdf4c264d503e8a1a364501db568438ff
Olamaryse <114025924+Olamaryse@users.noreply.github.com>
2023-01-29 22:47:21 +0000

    Function for getting a random element from an array

---
+++ script.js
@@ -0,0 +1,138 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+  ];
+  
+  // Array of numeric characters to be included in password
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  
+  // Array of lowercase characters to be included in password
+  var lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  
+  // Array of uppercase characters to be included in password
+  var upperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
+
+
+// Function to prompt user for password options
+  function getPasswordOptions(questions) {
+    let responses = [];
+    for (let i = 0; i < questions.length; i++) {
+      let response = confirm(questions[i]);
+      responses.push(response);
+    }
+    return responses;
+  }
+  
+  const question = ['would you like to include special characters?', 
+    'would you like to include numeric characters?',
+    'would you like to include lowercase characters?',
+    'would you like to include uppercase characters?'];
+  
+
+  let userResponses = getPasswordOptions(question);
+ 
+
+  // Function for getting a random element from an array
+
+    function getRandom(arr) {
+        let randomIndex = Math.floor(Math.random() * arr.length);
+        return arr[randomIndex];
+      }
+   
+
+  
+  // Function to generate password with user input
+  function generatePassword() {
+  
+  }
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector('#generate');
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+  
+    passwordText.value = password;
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener('click', writePassword);
+  
